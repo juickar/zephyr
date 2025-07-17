e0fc605777134b4388cf94f5b9de9b2eb28dccf0
Julien Racki <julien.racki@st.com>
2025-07-17 11:08:14 +0200

    soc: st: stm32: Provide basic support for the MP25 Cortex A35
    Enable basic support to the MP25 Cortex A35
    in single and dual core configuration
    with I and D cache enabled.
    
    Signed-off-by: Julien Racki <julien.racki@st.com>

---
+++ soc/st/stm32/stm32mp2x/a35/soc.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2025 STMicroelectronics
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file SoC configuration macros for the STM32MP25 family processors.
+ *
+ * Based on reference manual:
+ *   STM32MP257 advanced ARM(r)-based 32/64-bit MPUs
+ *
+ * Chapter 2.4.2: Memory map and register boundary addresses
+ */
+
+#ifndef _STM32MP25SOC_H_
+#define _STM32MP25SOC_H_
+
+#ifndef _ASMLANGUAGE
+
+#include <stm32mp2xx.h>
+
+#endif /* !_ASMLANGUAGE */
+
+#endif /* _STM32MP25SOC_H_ */
